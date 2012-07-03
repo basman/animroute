96a41b1f899d6b83fbbc577357eb54fdcf5c3343
Roman Hoog Antink <rha@disconnect.ch>
2012-07-03 07:41:06 +0200

    don't convert gradient to float if None

--- geometry.py
+++ geometry.py
@@ -12,10 +12,11 @@ def gradient(p1, p2):
 # special cases: m=None, so b represents x at y=0 instead of y at x=0
 class Line:
     def __init__(self, m, point):
-        self.m = float(m)
         if m == None:
+            self.m = m
             self.b = float(point[0]) # vertical
         else:
+            self.m = float(m)
             self.b = point[1] - float(m) * point[0]
 
     def crosspoint(self, line):
